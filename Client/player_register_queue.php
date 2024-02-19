bcddb36dc46006cb3c55914c86f9a77354a2f90b
MelvilPE <melvil.bouron@orange.fr>
2024-02-19 16:11:46 +0100

    Successfully unregister from queue

---
+++ Client/player_register_queue.php
@@ -0,0 +1,28 @@
+<?php
+    $unregister_request = "";
+    if (isset($_GET['player_name']))
+    {
+        $unregister_request = "?player_name=".$_GET['player_name']."&player_unregister=true";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="global-styles.css">
+    <title>PowerPlay4</title>
+</head>
+<body>
+    <div class="container center-items">
+        <div class="w-50 flex-column align-items-center justify-content-center">
+            <h1 class="text-primary text-center">Waiting for another player!</h1>
+            <br>
+            <a class="btn btn-primary w-100" href="http://powerplay4/Server/player_register_queue.php<?=$unregister_request;?>" role="button">Unregister from queue!</a>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
