ea4007774df29659ab7d8b8901fede1852d5a40a
MelvilPE <48417412+MelvilPE@users.noreply.github.com>
2024-02-22 19:03:13 +0100

    Small fix when registering in queue

--- Client/player_register_queue.php
+++ Client/player_register_queue.php
@@ -1,6 +1,6 @@
 <?php
     require_once $_SERVER['DOCUMENT_ROOT'].'/Common/globals.php';
-    require_once $_SERVER['DOCUMENT_ROOT'].'/Server/Fetch/get_party_id_from_player_id_hfile.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/Server/Fetch/get_party_id_from_player_id.php';
     
     $player_id = 0;
     if (isset($_COOKIE['player_id']))
